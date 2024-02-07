aea9ffc0c1c4a5926a871220e4b3030033717b36
Cyrus Goh <hello@cyrusgoh.com>
2024-02-07 17:56:53 +0800

    use swc with vite, clean up

--- app/electron.vite.config.ts
+++ app/electron.vite.config.ts
@@ -1,34 +1,14 @@
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
-import { resolve } from 'path'
 
 export default defineConfig({
   main: {
-    resolve: {
-      alias: {
-        '@': resolve('src/main'),
-        '@shared': resolve('src/shared'),
-        '@preload': resolve('src/preload'),
-      },
-    },
     plugins: [externalizeDepsPlugin()],
   },
   preload: {
-    resolve: {
-      alias: {
-        '@main': resolve('src/main'),
-        '@shared': resolve('src/shared'),
-      },
-    },
     plugins: [externalizeDepsPlugin()],
   },
   renderer: {
-    resolve: {
-      alias: {
-        '@': resolve('src/renderer'),
-        '@shared': resolve('src/shared'),
-      },
-    },
     plugins: [react()],
   },
 })
